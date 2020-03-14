414a98b4d1f789a5362cc0d7c386c12221410802
Sébastien DEU <sebastien@sdeu.fr>
2020-03-14 18:54:12 +0100

    initialize Section template

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,9 @@ require("@rails/activestorage").start()
 require("channels")
 
 
+import './semantic.min'
+import './progress'
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
---
+++ app/javascript/packs/progress.js
@@ -0,0 +1,7 @@
+$('.ip_usage').progress({
+    className: {
+        error: 'success',
+        success: 'error',
+        active: 'error'
+    }
+});
