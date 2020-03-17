d3289ea5cfb4e7babc59b810d0563ca69a8dd805
Pazema <rigonkmalk@gmail.com>
2020-03-17 23:04:37 +0100

    test(charts): add chart in head and call in section/show test

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,8 @@ require("channels")
 
 import './semantic.min'
 import './progress'
+import './loader'
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
