70a513e7a9b92c68e94c3ac03c0d3feed80c3071
Sébastien DEU <sebastien@sdeu.fr>
2020-03-22 00:30:55 +0100

    fix(sentry): use right address

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Raven.configure do |config|
-  config.dsn = 'https://5fb75d25f91e4265bdb126411b8c28b9:65c52a37f3be4598bfea096fac5c88f6@sentry.nf.svk.gs/4'
+  config.dsn = 'https://5fb75d25f91e4265bdb126411b8c28b9:65c52a37f3be4598bfea096fac5c88f6@sentry.app.svk.gs/4'
   config.environments = %w[development production]
 end
 
