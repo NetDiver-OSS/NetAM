6c96394378e2a739e22fb4c73aff0948821fce0f
Vincent FRICOU <vincent@fricouv.eu>
2020-04-05 15:38:45 +0200

    updt(job): substitute state with name

--- app/jobs/export_section_to_csv_job.rb
+++ app/jobs/export_section_to_csv_job.rb
@@ -3,27 +3,28 @@ class ExportSectionToCsvJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    #database_entries = Usage.where(section_id: args[0]).map { |usage| {
-    #                                                  ip: usage.ip_used,
-    #                                                  state: usage.state,
-    #                                                  fqdn: usage.fqdn,
-    #                                                  description: usage.description,
-    #                                                  section_name: usage.section.name
-    #                                                }}
     database_entries = Usage.where(section_id: args[0]).map { |usage| [
                                                       usage.section.name,
                                                       usage.ip_used,
                                                       usage.fqdn,
                                                       usage.description,
                                                       usage.state ]}
-    #Sidekiq.logger.info "Starting network process: #{database_entries}"
     attributes = %w{section_name, ip, fqdn, description, state}
     mycsv = CSV.generate(headers: true) do |csv|
       csv << attributes
       database_entries.each do |ip_usage|
+        if ip_usage[4] == 0
+          ip_usage[4] = 'Reserved'
+        elsif ip_usage[4] == 1
+          ip_usage[4] = 'Active'
+        elsif ip_usage[4] == 2
+          ip_usage[4] = 'Inactive'
+        elsif ip_usage[4] == 3
+          ip_usage[4] = 'DHCP'
+        end
         csv << ip_usage
       end
     end
-    puts mycsv.inspect
+    return mycsv
   end
 end
