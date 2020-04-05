ecfa97bfeb430b0f850d8ecb6a0e6dd11e96cd0f
Vincent FRICOU <vincent@fricouv.eu>
2020-04-05 15:22:33 +0200

    updt(job): generate csv

--- app/jobs/export_section_to_csv_job.rb
+++ app/jobs/export_section_to_csv_job.rb
@@ -3,26 +3,27 @@ class ExportSectionToCsvJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    database_entries = Usage.where(section_id: args[0]).map { |usage| {
-                                                      ip: usage.ip_used,
-                                                      state: usage.state,
-                                                      fqdn: usage.fqdn,
-                                                      description: usage.description,
-                                                      section_name: usage.section.name
-                                                    }}
+    #database_entries = Usage.where(section_id: args[0]).map { |usage| {
+    #                                                  ip: usage.ip_used,
+    #                                                  state: usage.state,
+    #                                                  fqdn: usage.fqdn,
+    #                                                  description: usage.description,
+    #                                                  section_name: usage.section.name
+    #                                                }}
+    database_entries = Usage.where(section_id: args[0]).map { |usage| [
+                                                      usage.section.name,
+                                                      usage.ip_used,
+                                                      usage.fqdn,
+                                                      usage.description,
+                                                      usage.state ]}
     #Sidekiq.logger.info "Starting network process: #{database_entries}"
     attributes = %w{section_name, ip, fqdn, description, state}
-    CSV.generate(headers: true) do |csv|
+    mycsv = CSV.generate(headers: true) do |csv|
       csv << attributes
-
-      puts "-------------  CSV INSPECTION ------------------"
-      puts csv.inspect
-
       database_entries.each do |ip_usage|
-        csv << ip_usage.values
+        csv << ip_usage
       end
-
-      puts csv.inspect
     end
+    puts mycsv.inspect
   end
 end
