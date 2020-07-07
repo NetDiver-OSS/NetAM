ff44237ba230a11c19175738917db4183d775f31
Sébastien DEU <sebastien@sdeu.fr>
2020-07-07 20:29:46 +0200

    feat: clean VERSION library for netam

--- lib/netam/version.rb
+++
@@ -1,9 +0,0 @@
-module Netam
-  class Version
-    @netam_version_file = 'NETAM_VERSION'
-
-    def self.current
-      @version ||= File.read(@netam_version_file).chomp if File.readable?(@netam_version_file)
-    end
-  end
-end
