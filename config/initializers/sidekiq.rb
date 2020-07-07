30822bad7ab8b970af32a6997dcaba02a6b95d0a
Vincent FRICOU <vincent@fricouv.eu>
2020-07-07 22:07:28 +0200

    updt: redis configuration for sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,8 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL") { 'redis://localhost:6379/0' } }
+  config.redis = { url: ENV.fetch("REDIS_URL") { "redis://#{Rails.configuration.netam[:redis][:host]}:#{Rails.configuration.netam[:redis][:port]}/#{Rails.configuration.netam[:redis][:db]}" } }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL") { 'redis://localhost:6379/0' } }
+  config.redis = { url: ENV.fetch("REDIS_URL") { "redis://#{Rails.configuration.netam[:redis][:host]}:#{Rails.configuration.netam[:redis][:port]}/#{Rails.configuration.netam[:redis][:db]}" } }
 end
 
