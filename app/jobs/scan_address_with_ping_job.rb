5d5e2872e21be10916a88bb1f54b576a7e66d5f0
Sébastien DEU <sebastien@sdeu.fr>
2020-07-13 17:03:32 +0200

    feat: optimize job ScanAddressWithPing

--- app/jobs/scan_address_with_ping_job.rb
+++ app/jobs/scan_address_with_ping_job.rb
@@ -6,21 +6,24 @@ class ScanAddressWithPingJob < ApplicationJob
 
   def perform(*args)
     raise 'Job can only process 1 network at the time' if args.count > 1
-    Sidekiq.logger.info "Starting address scan process: #{args[0][:ip_used].to_s}"
-    unless ['locked', 'dhcp'].include? args[0][:state]
-      if Net::Ping::External.new(args[0][:ip_used].to_s).ping?
-        Sidekiq.logger.info "IP #{args[0][:ip_used].to_s} is active"
-        addr_id = Usage.where(id: args[0][:id], ip_used: args[0][:ip_used].to_s, section_id: args[0][:section_id]).first_or_create
-        addr_id.update_attributes(
-          :state => :actived
-        )
-      else
-        Sidekiq.logger.info "IP #{args[0][:ip_used].to_s} is inactive"
-        addr_id = Usage.where(id: args[0][:id], ip_used: args[0][:ip_used].to_s, section_id: args[0][:section_id]).first_or_create
-        addr_id.update_attributes(
-          :state => :down
-        )
-      end
+
+    Sidekiq.logger.info "Starting address scan process: #{args[0][:ip_used]}"
+
+    if ['locked', 'dhcp'].include? args[0][:state]
+      Sidekiq.logger.error("#{self.class.name} is not able to process locked or dhcp address")
+      return
+    end
+
+    ping = Net::Ping::External.new(args[0][:ip_used].to_s).ping?
+
+    Sidekiq.logger.info "IP #{args[0][:ip_used]} is #{ping ? 'active' : 'inactive'}"
+
+    addr_id = Usage.where(id: args[0][:id], ip_used: args[0][:ip_used].to_s, section_id: args[0][:section_id]).first_or_create
+
+    if ping
+      addr_id.update_attributes(:state => :actived)
+    else
+      addr_id.update_attributes(:state => :down)
     end
   end
 end
