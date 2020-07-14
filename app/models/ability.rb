e42c25f28ae9c6f5110baf6d0a6fe00aa91e5a27
Sébastien DEU <sebastien@sdeu.fr>
2020-07-14 18:01:25 +0200

    feat: give app permissions to admins

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,7 +6,7 @@ class Ability
   def initialize(user)
     user ||= User.new
 
-    # can :manage, :all if user.admin?
+    can :manage, :all if user.admin?
 
     user.permissions.each do |permission|
       if permission.subject_id.nil?
