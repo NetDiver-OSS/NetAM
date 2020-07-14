68b0b9f2db4456c7946cad179a59728620854d07
Sébastien DEU <sebastien@sdeu.fr>
2020-07-14 11:18:50 +0200

    feat: first implementation of API endpoints

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,5 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+
+//= link grape_swagger_rails/application.css
+//= link grape_swagger_rails/application.js
---
+++ app/controllers/api/v1/base.rb
@@ -0,0 +1,15 @@
+module API
+  module V1
+    class Base < Grape::API
+      mount API::V1::Sections
+
+      add_swagger_documentation(
+          api_version: "v1",
+          hide_documentation_path: true,
+          mount_path: "/api/v1/swagger_doc",
+          hide_format: true
+      )
+
+    end
+  end
+end
