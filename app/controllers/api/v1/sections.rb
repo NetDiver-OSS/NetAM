c07c5ec9446c309bafb0b0199600bce5d700e45a
Sébastien DEU <sebastien@sdeu.fr>
2020-07-16 23:42:00 +0200

    feat: allow creation of section + list usage of section

--- app/controllers/api/v1/sections.rb
+++ app/controllers/api/v1/sections.rb
@@ -13,12 +13,31 @@ module API
           Section.all
         end
 
+        desc 'Create a section'
+        params do
+          requires :name, type: String
+          requires :network, type: String
+          requires :schedule, type: String
+          optional :description, type: String
+        end
+        post '', root: 'section' do
+          Section.create!(declared_params(include_missing: false))
+        end
+
         desc 'Return a section'
         params do
           requires :id, type: String, desc: 'ID of the section'
         end
         get ':id', root: 'section' do
           Section.where(id: permitted_params[:id]).first!
+        end
+
+        desc 'Return usage for a section'
+        params do
+          requires :id, type: String, desc: 'ID of the section'
+        end
+        get ':id/usages', root: 'section' do
+          Section.where(id: permitted_params[:id]).first!.usages
         end
       end
     end
