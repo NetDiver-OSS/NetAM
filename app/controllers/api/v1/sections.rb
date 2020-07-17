ad467d571a1f5e4ae5caccc5ff84c7c7bb677a84
Sébastien DEU <sebastien@sdeu.fr>
2020-07-17 20:22:51 +0200

    feat: implement creation of usage

--- app/controllers/api/v1/sections.rb
+++ app/controllers/api/v1/sections.rb
@@ -38,7 +38,7 @@ module API
         end
         post ':id/scan', root: 'section' do
           @section = Section.find(permitted_params[:id])
-          ScanNetworkWithPingJob.perform_later({id: @section, network: @section.network})
+          ScanNetworkWithPingJob.perform_later({ id: @section, network: @section.network })
 
           { status: 'ack' }
         end
@@ -48,7 +48,19 @@ module API
           requires :id, type: String, desc: 'ID of the section'
         end
         get ':id/usages', root: 'section' do
-          Section.where(id: permitted_params[:id]).first!.usages
+          Section.find(permitted_params[:id]).usages
+        end
+
+        desc 'Create a usage for a section'
+        params do
+          requires :id, type: String, desc: 'ID of the section'
+          requires :ip_used, type: String
+          requires :fqdn, type: String
+          requires :state, type: String
+          optional :description, type: String
+        end
+        post ':id/usages', root: 'section' do
+          Section.find(permitted_params[:id]).usages.create!(declared_params(include_missing: false).except(:id))
         end
       end
     end
