d2617ae0571da4d4c21a8469f1d79d7004eee574
Sébastien DEU <sebastien@sdeu.fr>
2020-07-20 22:38:43 +0200

    test: add tests for job export section

---
+++ spec/jobs/export_section_to_csv_job_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe ExportSectionToCsvJob, type: :job do
+  it "should return csv" do
+    section = Section.create!({ name: 'TU', network: '10.0.0.0/24', schedule: '24h' })
+    section.usages.create!({ ip_used: '10.0.0.250', fqdn: 'domain.com', state: 'locked' })
+    section.usages.create!({ ip_used: '10.0.0.251', fqdn: 'domain.com', state: 'actived' })
+    section.usages.create!({ ip_used: '10.0.0.252', fqdn: 'domain.com', state: 'down' })
+    section.usages.create!({ ip_used: '10.0.0.253', fqdn: 'domain.com', state: 'dhcp' })
+
+    expect(ExportSectionToCsvJob.perform_now(section.id)).to eq(
+      """ID,Section,Address,FQDN,Description,State
+#{section.id},TU,10.0.0.250,domain.com,,Reserved
+#{section.id},TU,10.0.0.251,domain.com,,Active
+#{section.id},TU,10.0.0.252,domain.com,,Inactive
+#{section.id},TU,10.0.0.253,domain.com,,DHCP
+"""
+    )
+  end
+end
