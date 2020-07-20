01b730aad35b9343d6be99d4c7e6b885683be485
Sébastien DEU <sebastien@sdeu.fr>
2020-07-20 23:17:18 +0200

    test: add tests for job scan network

---
+++ spec/jobs/scan_network_with_ping_job_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe ScanNetworkWithPingJob, type: :job do
+  it "should fail if multiple network is given" do
+    expect { ScanNetworkWithPingJob.perform_now('10.0.0.0/24', '192.168.0.0/24') }.to raise_error
+  end
+
+  it "should be nil if usage is locked" do
+    section = Section.create!({ name: 'section', network: '10.0.0.0/24', schedule: '24h' })
+
+    expect(ScanNetworkWithPingJob.perform_now({ id: section, network: section.network })).to be_truthy
+  end
+end
