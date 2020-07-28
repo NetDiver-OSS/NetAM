2fcc75cd1d35e11f238be7a907860140610fa0c1
Sébastien DEU <sebastien@sdeu.fr>
2020-07-28 21:16:37 +0200

    feat: use cancan on api

--- app/controllers/api/v1/base.rb
+++ app/controllers/api/v1/base.rb
@@ -4,9 +4,9 @@ module API
       mount API::V1::Sections
 
       add_swagger_documentation(
-        api_version: "requests.api.v1",
+        api_version: "v1",
         hide_documentation_path: true,
-        mount_path: "/requests.api/requests.api.v1/swagger_doc",
+        mount_path: "/api/v1/swagger_doc",
         hide_format: true
       )
     end
