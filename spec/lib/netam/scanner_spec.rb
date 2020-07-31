b839b61fe21ef62b74d89d6352d922b0506199ed
Sébastien DEU <sebastien@sdeu.fr>
2020-07-31 20:35:48 +0200

    chore: add verification of fail if scanner is not existant

--- spec/lib/netam/scanner_spec.rb
+++ spec/lib/netam/scanner_spec.rb
@@ -2,6 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Netam::Scanner do
   describe 'run' do
+    it 'should raise if job not exist' do
+      @section = Section.create!(
+        {
+          name: 'section 1',
+          network: '10.0.0.0/24',
+          schedule: '24 hours'
+        }
+      )
+
+      expect { Netam::Scanner.new('ScanNetworkWithNothingJob').run(@section.id, @section.network) }.to raise_error(NameError)
+    end
+
     it 'should return job_id' do
       @section = Section.create!(
         {
