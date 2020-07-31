d97ffe655f3c861e60fc07b171b828661bd6b2a0
Sébastien DEU <sebastien@sdeu.fr>
2020-07-31 22:15:11 +0200

    feat: import usage for section with CSV

--- app/models/usage.rb
+++ app/models/usage.rb
@@ -4,4 +4,18 @@ class Usage < ApplicationRecord
   enum state: %i[locked actived down dhcp]
 
   validates :state, :ip_used, presence: true
+
+  # @param [int] section_id
+  # @param [ActionDispatch::HTTP::UploadedFile] file
+  def self.import(section_id, file)
+    CSV.foreach(file.path, headers: true) do |row|
+      Usage.create!(
+        {
+          section_id: section_id,
+          ip_used: row['ip'],
+          state: row['state'].downcase.to_sym
+        }
+      )
+    end
+  end
 end
