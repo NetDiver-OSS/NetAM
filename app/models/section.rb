02dfbda33105d2cfbd8bfb21a4a63a06fcf3d740
Sébastien DEU <sebastien@sdeu.fr>
2020-08-01 16:02:56 +0200

    feat: add method to get the first unused ip

--- app/models/section.rb
+++ app/models/section.rb
@@ -4,6 +4,12 @@ class Section < ApplicationRecord
   validates :name, :network, :schedule, presence: true
   validate :network_must_be_valid, :schedule_must_be_cron
 
+  def request_unused_ip
+    IPAddress(network).hosts.each do |ip|
+      return ip.to_s if usages.where(ip_used: ip.to_s).empty?
+    end
+  end
+
   private
 
   def schedule_must_be_cron
