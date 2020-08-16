b2118421feed907bc693a2316941ab97b1295c6a
Sébastien DEU <sebastien@sdeu.fr>
2020-08-16 22:48:01 +0200

    fix: old authentication method in unit test

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -9,7 +9,7 @@ Doorkeeper.configure do
   end
 
   resource_owner_from_credentials do |routes|
-    user = User.find_for_database_authentication(email: params[:email])
+    user = User.find_for_database_authentication(email: params[:username])
     if user && user.valid_for_authentication? { user.valid_password?(params[:password]) }
       user
     end
