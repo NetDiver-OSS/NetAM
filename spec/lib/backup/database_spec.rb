b5d4522b4bd0bc476c25007dffcb535be637bfed
Sébastien DEU <sebastien@sdeu.fr>
2020-09-25 08:25:30 +0200

    fix: change backup path for CI

--- spec/lib/backup/database_spec.rb
+++ spec/lib/backup/database_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Backup::Database do
   it 'should run dump' do
-    ENV['BACKUP_PATH'] = './tmp/storage'
+    ENV['BACKUP_PATH'] = './'
     expect(Backup::Database.new.dump).to be_nil
   end
 end
