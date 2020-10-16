07f3b703f72500c77537533626aee8fd8122c3c4
Sébastien DEU <sebastien@sdeu.fr>
2020-10-16 23:00:32 +0200

    feat: display last datetime scan on section

---
+++ lib/netam/section.rb
@@ -0,0 +1,14 @@
+module NetAM
+  class Section
+    def self.get_last_scan(section_id)
+      Sidekiq.redis do |conn|
+        conn.scan_each(match: 'sidekiq:status:*', count: 100).map do |key|
+          status = Sidekiq::Status.get_all(key.split(':').last)
+          return status['update_time'] if JSON.parse(status['args']).first['id'] == section_id
+        end
+      end
+
+      0
+    end
+  end
+end
