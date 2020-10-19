5e45c28aa8df3842963dd1faf11a0a8871c4ff3c
Sébastien DEU <sebastien@sdeu.fr>
2020-10-19 20:00:03 +0200

    fix: update identifier field for usages

---
+++ db/migrate/20201019175343_update_identifier_for_usages.rb
@@ -0,0 +1,8 @@
+class UpdateIdentifierForUsages < ActiveRecord::Migration[6.0]
+  def change
+    Usage.where(identifier: nil).each do |usage|
+      usage.identifier = "#{usage.section_id}_#{usage.ip_used}"
+      usage.save!
+    end
+  end
+end
