f76411873aa00fb7ae28c08f436e7d4252235ebd
Vincent FRICOU <vincent@fricouv.eu>
2020-10-28 23:46:58 +0100

    add: enabling mail notifications

--- app/services/notifications/send_service.rb
+++ app/services/notifications/send_service.rb
@@ -7,6 +7,7 @@ module Notifications
     def call
       Notifications::Slack::SendService.call(@notification_object) if Rails.configuration.netam.dig(:notification, :slack_webhook).present?
       Notifications::Msteams::SendService.call(@notification_object) if Rails.configuration.netam.dig(:notification, :msteams_webhook).present?
+      Notifications::Mail::SendService.call(@notification_object) if Rails.configuration.netam.dig(:notification).present?
     end
   end
 end
