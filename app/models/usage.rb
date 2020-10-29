4610b77e1faf52c6a4b0df13a4b07ec4e64e429a
Sébastien DEU <sebastien@sdeu.fr>
2020-10-29 10:27:48 +0100

    refactor: clean model validation

--- app/models/usage.rb
+++ app/models/usage.rb
@@ -1,12 +1,19 @@
 require 'csv'
 
 class Usage < ApplicationRecord
+  after_initialize :init
+
   enum state: %i[locked actived down dhcp]
 
   belongs_to :section
 
-  validates :state, :ip_used, presence: true
+  validates :section, presence: true
+  validates :state, :ip_used, :identifier, presence: true
   validates :ip_used, uniqueness: { scope: :section, message: "should happen once per section" }
+
+  def init
+    self.identifier = "#{id}_#{ip_used}"
+  end
 
   # @param [int] section_id
   # @param [ActionDispatch::HTTP::UploadedFile] file
