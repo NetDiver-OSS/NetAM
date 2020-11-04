cf9811725d08cc9f3652d7e84155a17e240c021c
Sébastien DEU <sebastien@sdeu.fr>
2020-11-04 15:35:15 +0100

    feat: setup vue.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,6 @@
 const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
 
 const webpack = require('webpack')
 
@@ -12,4 +14,6 @@ environment.plugins.append('Define', new webpack.DefinePlugin({
   VERSION: JSON.stringify(require("../../package.json").version)
 }))
 
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
 module.exports = environment
