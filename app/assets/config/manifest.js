1d4e0dda10e1c19fd5131995079cf28462cc57a7
Sébastien DEU <sebastien@sdeu.fr>
2020-11-07 20:12:51 +0100

    feat: use translate in javascript files

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link_directory ../javascripts .js
 
 //= link grape_swagger_rails/application.css
 //= link grape_swagger_rails/application.js
