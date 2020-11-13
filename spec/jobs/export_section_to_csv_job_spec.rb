acb7c96d4bf20d0bca5794d460dffb6c59ab8920
Vincent FRICOU <vincent@fricouv.eu>
2020-11-13 16:40:24 +0100

    feat(export): uniformize export with import format

--- spec/jobs/export_section_to_csv_job_spec.rb
+++ spec/jobs/export_section_to_csv_job_spec.rb
@@ -12,9 +12,9 @@ RSpec.describe ExportSectionToCsvJob, type: :job do
 
     expect(ExportSectionToCsvJob.perform_now(section.id)).to eq(
       """ID,Section,Address,FQDN,Description,State
-#{section.id},TU,10.0.0.250,domain.com,,Reserved
-#{section.id},TU,10.0.0.251,domain.com,,Active
-#{section.id},TU,10.0.0.252,domain.com,,Inactive
+#{section.id},TU,10.0.0.250,domain.com,,LOCKED
+#{section.id},TU,10.0.0.251,domain.com,,ACTIVED
+#{section.id},TU,10.0.0.252,domain.com,,DOWN
 #{section.id},TU,10.0.0.253,domain.com,,DHCP
 """
     )
