4a2d67dac9b2d3ec0a6352b66931e5920bf966dd
Vincent FRICOU <vincent@fricouv.eu>
2020-11-18 10:24:20 +0100

    fix: remove worker validation break worker affectation

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -4,7 +4,5 @@ class Worker < ApplicationRecord
   has_many :sections, dependent: :nullify
 
   validates_associated :sections
-  validates :id, :name, presence: true
-  validates :id, numericality: true
-  validates :vid, uniqueness: true
+  validates :name, presence: true
 end
