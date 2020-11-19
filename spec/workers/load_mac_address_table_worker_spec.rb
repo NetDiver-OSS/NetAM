23521fd470ab8d00b62d6aa2090c76aafc37fba4
Vincent FRICOU <vincent@fricouv.eu>
2020-11-19 23:36:29 +0100

    test: adding UT for OUI file download

--- spec/workers/load_mac_address_table_worker_spec.rb
+++ spec/workers/load_mac_address_table_worker_spec.rb
@@ -2,5 +2,13 @@
 
 require 'rails_helper'
 RSpec.describe LoadMacAddressTableWorker, type: :worker do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should fail if unable to download file' do
+    expect do
+      described_class.new.perform('oui.json')
+    end.to raise_error(StandardError, 'Unable to download MAC Address database file')
+  end
+
+  it 'should download file' do
+    expect { described_class.new.perform }.not_to raise_error
+  end
 end
