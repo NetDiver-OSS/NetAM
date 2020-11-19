31e7f8613e5871f4c32a29014b5f962932b3b67c
Vincent FRICOU <vincent@fricouv.eu>
2020-11-19 23:19:28 +0100

    fix: variabilize file name

--- app/workers/load_mac_address_table_worker.rb
+++ app/workers/load_mac_address_table_worker.rb
@@ -1,14 +1,15 @@
+# frozen_string_literal: true
+
 class LoadMacAddressTableWorker
   include Sidekiq::Worker
 
-  def perform(*args)
-    # Do something
-    response = Faraday.get 'http://standards-oui.ieee.org/oui/oui.txt'
+  def perform(file_name = 'oui.txt')
+    response = Faraday.get "http://standards-oui.ieee.org/oui/#{file_name}"
     if response.status == 200
-      output_file = File.new(Rails.root.join('tmp/oui.txt'), 'w')
+      Sidekiq.logger.debug("Writing downloaded content to file as temporary file #{file_name}")
+      output_file = File.new(Rails.root.join("tmp/#{file_name}"), 'w')
       output_file.syswrite(response.body)
-    else
-      Sidekiq.logger.error('Unable to download MAC Address database file')
     end
+    raise StandardError, 'Unable to download MAC Address database file' unless response.status == 200
   end
 end
