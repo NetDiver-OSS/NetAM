cd50e79f776553dd4e03856b34379edf8388ddfc
Sébastien DEU <sebastien@sdeu.fr>
2020-11-20 13:13:52 +0100

    feat: add design for mac vendor lookup

--- app/controllers/utils_controller.rb
+++ app/controllers/utils_controller.rb
@@ -6,8 +6,6 @@ class UtilsController < ApplicationController
   end
 
   def mac_vendor
-    mac_prefix_table = params[:mac].split(':')
-    mac_prefix = "#{mac_prefix_table[0]}:#{mac_prefix_table[1]}:#{mac_prefix_table[2]}"
-    @mac_vendor = MacAddress.where('mac = :mac', mac: mac_prefix)
+    @mac_vendor = MacAddress.find_by(mac: params[:mac].split(':').take(3).join(':').upcase) rescue nil
   end
 end
