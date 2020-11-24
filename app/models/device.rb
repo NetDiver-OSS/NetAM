2fac91a25ae9594b23b60fc7d9825985d68be9ae
Vincent FRICOU <vincent@fricouv.eu>
2020-11-24 21:07:25 +0100

    updt: page style

--- app/models/device.rb
+++ app/models/device.rb
@@ -2,7 +2,7 @@
 
 class Device < ApplicationRecord
   belongs_to :device_type, optional: true
-  delegate :name, to: :device_type, prefix: true, allow_nil: true
+  delegate :name, :color, to: :device_type, prefix: true, allow_nil: true
 
   validates :name, presence: true, uniqueness: true
 end
