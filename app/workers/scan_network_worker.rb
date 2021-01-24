f53bbfdcfd124f756182155a9750cd925b61efd7
Sébastien DEU <sebastien@sdeu.fr>
2021-01-24 15:06:31 +0100

    refactor: optimize network scan worker

--- app/workers/scan_network_worker.rb
+++ app/workers/scan_network_worker.rb
@@ -21,28 +21,25 @@ class ScanNetworkWorker
     Sidekiq.logger.info "Starting network process: #{section[:network]}"
 
     @section_usage = Usage.where(section_id: section[:id]).select(:ip_used, :state, :fqdn).to_a
-    section_to_update = []
 
     Parallel.each(section[:network].to_range, in_threads: Rails.configuration.netam[:sidekiq][:parallel]) do |address|
+      current_usage = { ip_used: address.to_s, section_id: section[:id] }
       usage = {
         state: @section_usage.filter_map { |entry| entry.state if entry.ip_used.to_s == address.to_s },
         fqdn: @section_usage.filter_map { |entry| entry.fqdn if entry.ip_used.to_s == address.to_s }
       }
 
       if SKIPPED_STATE.include? usage[:state].first
-        Sidekiq.logger.info "Address #{address} is not able to be process if state is locked or dhcp"
+        Sidekiq.logger.info "Address #{address} is not able to be process if state is: #{SKIPPED_STATE.join(', ')}"
         next
       end
 
       scanner = { ping: "NetAM::Scanner::#{section[:scan_type].to_s.titleize.delete(' ')}".constantize.new(address.to_s).scan! }
 
-      current_usage = { identifier: "#{section[:id]}_#{address}", ip_used: address.to_s, section_id: section[:id] }
-
       if scanner[:ping]
         Sidekiq.logger.info usage[:state].count.positive? ? "Known active IP: #{address}" : "Found new active IP: #{address}"
 
         current_usage[:state] = :actived
-
         scanner[:reverse] = NetAM::Network::Dns.reverse_dns(address)
 
         unless scanner[:reverse].nil?
@@ -56,19 +53,15 @@ class ScanNetworkWorker
         current_usage[:state] = :down
       end
 
-      section_to_update << current_usage if current_usage.length > 3
+      Usage.find_or_create_by(identifier: "#{section[:id]}_#{address}").update!(current_usage) if current_usage.length > 2
     end
 
-    # Fix error "All objects being inserted must have the same keys"
-    # Need rework...
-    unless section_to_update.empty?
-      lite_section = section_to_update.dup.keep_if { |h| h.length == 4 }
-      full_section = section_to_update.dup.keep_if { |h| h.length == 5 }
+    send_notification(section)
+  end
 
-      Usage.upsert_all(lite_section, unique_by: %i[identifier]) unless lite_section.empty?
-      Usage.upsert_all(full_section, unique_by: %i[identifier]) unless full_section.empty?
-    end
+  private
 
-    Notifications::SendService.call({ section: section, message: 'Scan finished !' }) if Section.find(section[:id]).settings(:notification).on_run
+  def send_notification(section)
+    Notifications::SendService.call({ section: section, message: _('Scan finished !') }) if Section.find(section[:id]).settings(:notification).on_run
   end
 end
