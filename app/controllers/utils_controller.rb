67bdf5b63723a20935fac01cfbb00b3c7d5021ad
Vincent FRICOU <vincent@fricouv.eu>
2021-03-26 08:29:24 +0100

    feat: add tools for PTR resolution

--- app/controllers/utils_controller.rb
+++ app/controllers/utils_controller.rb
@@ -8,4 +8,8 @@ class UtilsController < ApplicationController
   def mac_vendor
     @mac_vendors = MacAddress.where(mac: params[:mac].split(':').take(3).join(':').upcase) rescue nil
   end
+
+  def ptr_resolution
+    @ptr_resolutions = NetAM::Network::Dns.reverse_dns(params[:address]) rescue nil
+  end
 end
