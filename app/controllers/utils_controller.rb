3d84857852697f65569c0269425869cfbe034424
Vincent FRICOU <vincent@fricouv.eu>
2021-03-26 09:52:51 +0100

    feat: add tools network range splitter

--- app/controllers/utils_controller.rb
+++ app/controllers/utils_controller.rb
@@ -9,6 +9,12 @@ class UtilsController < ApplicationController
     @mac_vendors = MacAddress.where(mac: params[:mac].split(':').take(3).join(':').upcase) rescue nil
   end
 
+  def network_splitter
+    return if params[:network].nil?
+    network_range = IPAddress(params[:network].to_s)
+    @networks = network_range / params[:split_count].to_i
+  end
+
   def ptr_resolution
     @ptr_resolutions = NetAM::Network::Dns.reverse_dns(params[:address]) rescue nil
   end
