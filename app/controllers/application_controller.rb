f3b2a9ab44243dc77ac12e5fd2ca94c99ab67cfb
Sébastien DEU <sebastien@deu.sh>
2021-06-30 13:44:24 +0200

    fix: use protect_from_forgery with exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   before_action :set_gettext_locale
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
-  protect_from_forgery
+  protect_from_forgery with: :exception
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, alert: exception.message
